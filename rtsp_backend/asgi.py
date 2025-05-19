a558ff6f14706f7d1fd5c6a3c64f18355c89d645
harshkhavale <harshkhavale1102@gmail.com>
2025-05-19 20:08:13 +0530

    feat:application setup essentials

--- rtsp_backend/asgi.py
+++ rtsp_backend/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for rtsp_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import stream.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rtsp_backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            stream.routing.websocket_urlpatterns
+        )
+    ),
+})
